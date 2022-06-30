3cb72fb8acb8096ebe494eda69f70b5cfcb36dd8
Fabio Macieira <fabiomacieira16@gmail.com>
2022-06-30 18:16:13 -0300

    feature: final componet update

--- crud/frontend/src/app/components/product/product-update/product-update.component.ts
+++ crud/frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,3 +1,6 @@
+import { Product } from './../product.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-update.component.css']
 })
 export class ProductUpdateComponent implements OnInit {
+  product: Product;
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+
+  ) { }
 
   ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.productService.readById(id).subscribe((prod) => {
+      this.product = prod;
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMenssage("Produto atualizado com sucesso!");
+      this.router.navigate(["/products"])
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(["/products"])
   }
 
 }
